8b593f39ae8e686710cb0c2e60d3d82c8fc64d34
Jose Cisneros <jcis494@gmail.com>
2015-11-09 20:25:39 -0600

    First Commit

---
+++ RegexToNFA.rb
@@ -0,0 +1,10 @@
+require './RPNConverter.rb'
+
+converter = RPNConverter.new()
+
+puts "Kleene Star'*'\n Klenne Star not null '+'\nUnion '|'\nConcatenation '.'"
+puts "Enter Regular Expression"
+regex = "(1.1)|(0+.1)"
+resultado = blah.infix_to_rpn(regex)
+puts resultado
+
